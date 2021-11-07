fe583797cb0e4cfbd2b6cef8d83586abea90a63f
hachaniwassim <wassimhach16@gmail.com>
2021-11-07 13:31:01 +0100

    pushing classes

---
+++ mobile/lib/classes/utilisateur.dart
@@ -0,0 +1,64 @@
+class Utilisateur {
+  String _nom;
+  String _prenom;
+  String _tel;
+  String _adresse;
+
+  Utilisateur(this._nom, this._prenom, this._tel, this._adresse);
+
+  String get adresse => _adresse;
+
+  set adresse(String value) {
+    _adresse = value;
+  }
+
+  String get tel => _tel;
+
+  set tel(String value) {
+    _tel = value;
+  }
+
+  String get prenom => _prenom;
+
+  set prenom(String value) {
+    _prenom = value;
+  }
+
+  String get nom => _nom;
+
+  set nom(String value) {
+    _nom = value;
+  }
+}
+class utilisateur {
+  String _nom;
+  String _prenom;
+  String _tel;
+  String _adresse;
+
+  utilisateur(this._nom, this._prenom, this._tel, this._adresse);
+
+  String get adresse => _adresse;
+
+  set adresse(String value) {
+    _adresse = value;
+  }
+
+  String get tel => _tel;
+
+  set tel(String value) {
+    _tel = value;
+  }
+
+  String get prenom => _prenom;
+
+  set prenom(String value) {
+    _prenom = value;
+  }
+
+  String get nom => _nom;
+
+  set nom(String value) {
+    _nom = value;
+  }
+}
