44954dd20c4a7e889bdd83e082190e8d8653ffbb
hachaniwassim <wassimhach16@gmail.com>
2021-11-08 13:06:59 +0100

    generating not found component&redirection to home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { VoitureComponent } from './voiture/voiture.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 const routes : Routes =[
 
   { path : "home", component : HomeComponent},
-  { path : "voiture", component : VoitureComponent}
+  { path : "voiture", component : VoitureComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path : "**", component : NotFoundComponent}
+
 ]
   
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotFoundComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
