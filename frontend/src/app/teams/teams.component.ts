356bdfa864cf5fd010e0a8cf9003edbcf9093779
abirtarchoun <abirtarchoun.8@gmail.com>
2021-11-28 00:23:52 +0100

    add routing teams

--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { teams } from './teams-list';
+import { Teams } from './teams.model';
 
 @Component({
   selector: 'app-teams',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teams.component.css']
 })
 export class TeamsComponent implements OnInit {
+  public teamsList: Teams[] = teams;
 
   constructor() { }
 
