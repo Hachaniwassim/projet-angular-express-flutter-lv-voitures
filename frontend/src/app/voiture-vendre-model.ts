baaa2c4b22c8351c88f63251fac6eb9ee7b821d1
rayen-naffouti <naffoutirayen11@gmail.com>
2021-11-27 21:51:41 +0100

    read all voiture a vendre

---
+++ frontend/src/app/voiture-vendre-model.ts
@@ -0,0 +1,12 @@
+export interface voiture_vendre {
+    id: string;
+    title: string;
+    description: string;
+    image: string;
+    last_update: string;
+    instructor: string;
+    language: string;
+    price: number;
+    goals: string[];
+    requirements: string[];
+  }
