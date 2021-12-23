61f97939d7b1ab6aad20413d0b3ff518e007cab2
hachaniwassim <wassimhach16@gmail.com>
2021-12-23 01:31:25 +0100

    favorites-rent component &fixing favorites routing

---
+++ frontend/src/app/favorites/favorites-rent/favorites-rent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-rent',
+  templateUrl: './favorites-rent.component.html',
+  styleUrls: ['./favorites-rent.component.css']
+})
+export class FavoritesRentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/favorites/favorites-routing.module.ts
+++ frontend/src/app/favorites/favorites-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FavoritesComponent } from './favorites.component';
+import { FavoritesRentComponent } from './favorites-rent/favorites-rent.component';
 
-const routes: Routes = [{ path: '', component: FavoritesComponent }];
+const routes: Routes = [
+  
+  { path: '', component: FavoritesComponent },
+  { path: 'rent-favourites', component: FavoritesRentComponent }
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    FavoritesRentComponent
+  ]
 })
 export class FavoritesRoutingModule { }
