a14f39ed6e21ed350d40e9fd2f20815d04c11d75
abirtarchoun <abirtarchoun.8@gmail.com>
2021-12-24 17:56:28 +0100

    create client-list + clientmodel + feedback-details

--- frontend/src/app/feedback/feedback-routing.module.ts
+++ frontend/src/app/feedback/feedback-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedbackComponent } from './feedback.component';
+import { FeedbackDetailsComponent } from './feedback-details/feedback-details.component';
 
-const routes: Routes = [{ path: '', component: FeedbackComponent }];
+const routes: Routes = [{ path: '', component: FeedbackComponent },
+
+{ path: ':id', component: FeedbackDetailsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    FeedbackDetailsComponent
+  ]
 })
 export class FeedbackRoutingModule { }
