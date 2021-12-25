257a64ed92e4f59471f523759ee668b28e1fa8e7
abirtarchoun <abirtarchoun.8@gmail.com>
2021-12-25 18:23:23 +0100

    update

--- backend/controllers/rent.js
+++ backend/controllers/rent.js
@@ -1,37 +1,40 @@
-const Voiture = require('./../models/voiture_rent');
+const Voiture = require('./../models/Rent');
 
 
-// get all  rent cars
-router.get('/', (req, res) => {
-    Voiture.find()
-      .then(voitures => res.status(200).json(voitures))
-      .catch(err => res.status(400).json({error: err.message}));
+// get all 
+exports.all = (req, res) => {
+  Voiture.find()
+    .then( voitures => res.status(200).json( voitures))
+    .catch(err => res.status(400).json({error: err.message}));
+};
+
+// get  by id
+exports.get = (req, res, next) => {
+  Voiture.findOne({ _id: req.params.id })
+      .then( voiture => res.status(200).json( voiture))
+      .catch(error => res.status(404).json({ error }));
+  };
+
+  // store a new 
+exports.create = (req, res, next) => {
+  const voiture = new  Voiture({
+    ...req.body
   });
-  // get a rent car by id
-  router.get('/:id', (req, res, next) => {
-      Voiture.findOne({ _id: req.params.id })
-          .then(voiture => res.status(200).json(voiture))
-          .catch(error => res.status(404).json({ error }));
-      });
-      // store a new  rent car
-  router.post('/', (req, res, next) => {
-      const voiture = new Voiture({
-        ...req.body
-      });
-      course.save()
-        .then(() => res.status(201).json({ message: 'Car created  !'}))
-        .catch(error => res.status(400).json({ error }));
-    });
-    // update a rent car by id
-  router.put('/:id', (req, res, next) => {
-      voiture.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Car updated !'}))
-        .catch(error => res.status(400).json({ error }));
-    });
-    
-    // delete a rent car by id
-  router.delete('/:id', (req, res, next) => {
-      Voiture.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Car deleted !'}))
-        .catch(error => res.status(400).json({ error }));
-    });
+  Voiture.save()
+    .then(() => res.status(201).json({ message: 'Voiture  created  !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+// update a course by id
+exports.update = (req, res, next) => {
+  Voiture.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Voiture updated !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+// delete  by id
+exports.delete = (req, res, next) => {
+  Voiture.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Voiture  deleted !'}))
+    .catch(error => res.status(400).json({ error }));
+};
