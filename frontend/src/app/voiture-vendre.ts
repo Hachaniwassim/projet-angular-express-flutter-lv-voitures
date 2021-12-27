a42d504cc15181d8d197b97bc56ff2caa3070c61
rayen-naffouti <naffoutirayen11@gmail.com>
2021-12-27 22:42:04 +0100

    crud avec mongo et express

--- frontend/src/app/voiture-vendre.ts
+++ frontend/src/app/voiture-vendre.ts
@@ -7,7 +7,7 @@ export const voitures_vendre: voiture_vendre[] = [
     "description": "La Hyundai i10 est une automobile du constructeur automobile coréen Hyundai produite depuis 2008. cest une citadine 5 portes, elle a remplacé la Hyundai Atos Prime en 2008 sur le marché européen, mais pas en Inde, où Atos Prime, appelée Santro, continue à être produite et coûte moins cher",
     "image": "https://s38.wheelsage.org/format/picture/picture-preview-large/h/hyundai/grand_i10_sedan/hyundai_grand_i10_sedan_58.jpg",
     "Kilometrage": 10000,
-    "price": 33.99
+    "price": 3.99
   },
   {
     "id": "2",
@@ -15,7 +15,7 @@ export const voitures_vendre: voiture_vendre[] = [
     "description": "La Golf VIII est une berline compacte qui est la 8e génération de Volkswagen Golf produite à plus de 35 millions exemplaires depuis 1974 par le constructeur automobile allemand Volkswagen, et commercialisée à partir de mars 2020. Elle succède à la Golf VII produite de 2012 à 2019.",
     "image": "https://sf2.auto-moto.com/wp-content/uploads/sites/9/2019/10/volkswagen-golf-8-1-615x410.jpg",
     "Kilometrage": 34000,
-    "price": 55.00
+    "price": 5.00
   },
   {
     "id": "3",
