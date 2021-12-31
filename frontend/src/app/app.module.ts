946df5853a79673a63f7ff621531968e946a1d0d
rayen-naffouti <naffoutirayen11@gmail.com>
2021-12-31 17:50:56 +0100

    login + register (form) work

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FooterComponent } from './footer/footer.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MatConfirmDialogComponent } from './mat-confirm-dialog/mat-confirm-dialog.component';
 import { TeamsAdminComponent } from './teams-admin/teams-admin.component';
 import { TeamAddComponent } from './teams-admin/team-add/team-add.component';
@@ -21,6 +21,7 @@ import { AgencysAdminComponent } from './agencys-admin/agencys-admin.component';
 import { AgencyAddComponent } from './agencys-admin/agency-add/agency-add.component';
 import { AgencyListComponent } from './agencys-admin/agency-list/agency-list.component';
 import { DatePipe } from '@angular/common';
+import { AuthInterceptor } from "./auth.interceptor";
 
 
 @NgModule({
@@ -51,7 +52,14 @@ import { DatePipe } from '@angular/common';
     HttpClientModule
   ],
   
-  providers: [{provide: MatDialogRef, useValue: {close: (_dialogResult: any) => { }} },DatePipe],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {provide: MatDialogRef, useValue: {close: (_dialogResult: any) => { }} },
+    DatePipe],
   bootstrap: [AppComponent],
   entryComponents: [TeamAddComponent]
 })
