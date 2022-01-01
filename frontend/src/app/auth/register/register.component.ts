39351e50a92c67b5fe31738ddeb47067e0d9c9f2
abirtarchoun <abirtarchoun.8@gmail.com>
2022-01-01 22:18:09 +0100

    add nodemailing but still not work at all

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 import { Router } from '@angular/router';
 
 import { AuthService } from "src/app/shared/auth.service";
+import { MailService } from 'src/app/shared/mail.service';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +12,13 @@ import { AuthService } from "src/app/shared/auth.service";
 })
 export class RegisterComponent implements OnInit {
   public registerForm: FormGroup;
+  loading = false;
+  buttionText = "Submit";
 
   constructor(
     public formBuilder: FormBuilder,
     public authService: AuthService,
-    public router: Router
+    public router: Router , public http :MailService
   ) {
     this.registerForm = this.formBuilder.group({
       name: [''],
@@ -29,12 +32,42 @@ export class RegisterComponent implements OnInit {
   }
 
   signupUser() {
+    this.loading = true;
+    this.buttionText = "Submiting...";
+    let user = {
+      name: this.registerForm.value,
+      email: this.registerForm.value
+    }
     this.authService.signup(this.registerForm.value).subscribe(res => {
       if(res.status == 201) {
+        
         this.registerForm.reset();
         this.router.navigate(['/auth/login']);
+
+        this.http.sendEmail("http://localhost:3000/sendmail", user).subscribe(
+          data => {
+            let res:any = data; 
+            console.log(
+              `👏 > 👏 > 👏 > 👏 ${user.name} is successfully register and mail has been sent and the message id is ${res.messageId}`
+            );
+          },
+          err => {
+            console.log(err);
+            this.loading = false;
+            this.buttionText = "Submit";
+          },() => {
+            this.loading = false;
+            this.buttionText = "Submit";
+          }
+        );
+        
       }
     });
+
+   
   }
 
-}
+  }
+
+
+
