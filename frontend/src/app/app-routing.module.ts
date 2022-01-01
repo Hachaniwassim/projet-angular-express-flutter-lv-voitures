30adc438770a5c34981c72a178bb664e03e0c190
abirtarchoun <abirtarchoun.8@gmail.com>
2022-01-01 18:28:11 +0100

    add auth guard controle acces

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ContactComponent } from './contact/contact.component';
 import { MaterialModule } from './material/material.module';
 import { TeamsAdminComponent } from './teams-admin/teams-admin.component';
 import { AgencysAdminComponent } from './agencys-admin/agencys-admin.component';
+import { AuthGuard } from './guard/auth.guard';
 const routes : Routes =[
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -15,9 +16,9 @@ const routes : Routes =[
   { path : "services" ,component :ServicesComponent},
  // { path : "about", component: AboutComponent},
    { path :  "contact",component: ContactComponent}, 
-   {path : 'team', component: TeamsAdminComponent},
+   {path : 'team', component: TeamsAdminComponent,canActivate: [AuthGuard]},
    
-   {path : 'agencyAdmin', component: AgencysAdminComponent},
+   {path : 'agencyAdmin', component: AgencysAdminComponent,canActivate: [AuthGuard]},
 
 
  
@@ -33,7 +34,7 @@ const routes : Routes =[
 
   { path: 'feedback', loadChildren: () => import('./feedback/feedback.module').then(m => m.FeedbackModule) },
 
-  { path: 'cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) },
+  { path: 'cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) ,canActivate: [AuthGuard]},
 
   { path: 'favorites', loadChildren: () => import('./favorites/favorites.module').then(m => m.FavoritesModule) },
 
@@ -43,13 +44,13 @@ const routes : Routes =[
 
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
 
-  { path: 'rent-add', loadChildren: () => import('./rent-add/rent-add.module').then(m => m.RentAddModule) },
+  { path: 'rent-add', loadChildren: () => import('./rent-add/rent-add.module').then(m => m.RentAddModule) ,canActivate: [AuthGuard]},
 
-  { path: 'rdv-admin', loadChildren: () => import('./rdv-admin/rdv-admin.module').then(m => m.RdvAdminModule) },
+  { path: 'rdv-admin', loadChildren: () => import('./rdv-admin/rdv-admin.module').then(m => m.RdvAdminModule),canActivate: [AuthGuard] },
 
-  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule),canActivate: [AuthGuard] },
 
-  { path: 'streetmap', loadChildren: () => import('./streetmap/streetmap.module').then(m => m.StreetmapModule) },
+  { path: 'streetmap', loadChildren: () => import('./streetmap/streetmap.module').then(m => m.StreetmapModule),canActivate: [AuthGuard] },
 
  //notfound
 
